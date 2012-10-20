d87dda6e76a17332797eb675dd57278e49aabf59
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2012-10-20 16:45:33 +1000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoideployShowcase::Application.config.secret_token = 'fae3e1b4db5f12da024aab57f2c52ac797bf620cddacece9387fd3cc93e7beff22ee2d2ed97b721776b80098487666b292a21896691f84971fd6c9a26e8cfa4b'
