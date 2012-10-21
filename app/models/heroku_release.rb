37933681d609fdafe83713fe90fc4169c442d88c
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2012-10-21 18:36:57 +1000

    Enrich heroku releases with build data from travis ci.

--- app/models/heroku_release.rb
+++ app/models/heroku_release.rb
@@ -1,12 +1,19 @@
 class HerokuRelease < OpenStruct
   HEROKU_APP_NAME = 'koideploy-showcase'
   VALID_KEYS = %w(descr commit created_at name)
+  TRAVIS_CI_BUILDS_API_URL = 'https://travis-ci.org/koideploy/showcase/builds.json'
+  TRAVIS_CI_BUILD_BASE_URL = 'https://travis-ci.org/#!/koideploy/showcase/builds'
 
   class << self
     def get_releases
+      @travis_ci_builds = nil
       heroku.get_releases(HEROKU_APP_NAME).body.map do |release|
         new(get_attributes(release))
       end
+    end
+
+    def get_travis_ci_builds
+      @travis_ci_builds ||= HTTParty.get(TRAVIS_CI_BUILDS_API_URL)
     end
 
     private
@@ -23,5 +30,27 @@ class HerokuRelease < OpenStruct
 
   def created_at
     Time.parse super
+  end
+
+  def commit_message
+    build_for_commit['message'] if build_for_commit
+  end
+
+  def travis_ci_build_number
+    build_for_commit['number'] if build_for_commit
+  end
+
+  def travis_ci_build_url
+    if build_for_commit
+      [TRAVIS_CI_BUILD_BASE_URL, build_for_commit['id']].join('/')
+    end
+  end
+
+  private
+
+  def build_for_commit
+    @build_for_commit ||= self.class.get_travis_ci_builds.detect do |build|
+      build['commit'] =~ %r{^#{commit}}
+    end
   end
 end
--- spec/models/heroku_release_spec.rb
+++ spec/models/heroku_release_spec.rb
@@ -35,6 +35,33 @@ describe HerokuRelease do
       its(:env) { should == nil }
       its(:addons) { should == nil }
       its(:pstable) { should == nil }
+
+      context 'enrich with travis ci data' do
+        let(:travis_response) do
+          {
+            "id"=>2873143,
+            "repository_id"=>294130,
+            "number"=>"15",
+            "state"=>"finished",
+            "result"=>0,
+            "started_at"=>"2012-10-21T07:17:30Z",
+            "finished_at"=>"2012-10-21T07:19:13Z",
+            "duration"=>103,
+            "commit"=>"dfed7b994a4cd59d3bdac5bafb7f34ecb45e5646",
+            "branch"=>"master",
+            "message"=>"List the 10 recent heroku releases.",
+            "event_type"=>"push"
+          }
+        end
+
+        before do
+          HerokuRelease.stub(:get_travis_ci_builds).and_return([travis_response])
+        end
+
+        its(:commit_message) { should == 'List the 10 recent heroku releases.' }
+        its(:travis_ci_build_number) { should == '15' }
+        its(:travis_ci_build_url) { should == 'https://travis-ci.org/#!/koideploy/showcase/builds/2873143' }
+      end
     end
   end
 end
