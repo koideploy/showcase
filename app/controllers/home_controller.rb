60a08b694a4cd59d3bdac5bafb7f34ecb45e5646
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2012-10-21 17:15:00 +1000

    List the 10 recent heroku releases.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @releases = HerokuRelease.get_releases.reverse[0..10]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def commit_link(commit_sha)
+    link_to commit_sha, "https://github.com/koideploy/showcase/commit/#{commit_sha}", :target => '_blank', :class => 'commit'
+  end
 end
