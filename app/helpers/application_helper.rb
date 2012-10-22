e0650243468166c816ba1cc7fc6ac6992a9c1c15
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2012-10-22 22:48:06 +1000

    Link to license for more infos.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     link_to commit_sha, "https://github.com/koideploy/showcase/commit/#{commit_sha}", :target => '_blank', :class => 'commit'
   end
 
+  def license_link(photo)
+    link_to @photo[:license_name], @photo[:license_url], :class => 'license', :target => '_blank'
+  end
+
   def flickr_photo_backlink(photo)
     link_text = "#{@photo[:username]} - \"#{@photo[:title]}\""
     link_to link_text, @photo[:url_photopage], :target => '_blank'
