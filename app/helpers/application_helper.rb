781166ca067921f8c59735e0c5acb295d3a8f767
Sebastian Korfmann <korfmann.sebastian@gmail.com>
2012-10-22 22:47:41 +1000

    Backlink to flickr photo page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def commit_link(commit_sha)
     link_to commit_sha, "https://github.com/koideploy/showcase/commit/#{commit_sha}", :target => '_blank', :class => 'commit'
   end
+
+  def flickr_photo_backlink(photo)
+    link_text = "#{@photo[:username]} - \"#{@photo[:title]}\""
+    link_to link_text, @photo[:url_photopage], :target => '_blank'
+  end
 end
